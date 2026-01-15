442a8e76cce2ebecc349f04759d1beae877d4d9c
Sai <saibhagatsr@gmail.com>
2026-01-16 00:41:32 +0800

    Remove migrations

--- prisma/migrations/20251217121714_add_user_relations_to_loan_request/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "loan_request" ADD COLUMN     "new_loggie_id" INTEGER,
-ADD COLUMN     "new_req_id" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "loan_request" ADD CONSTRAINT "loan_request_new_req_id_fkey" FOREIGN KEY ("new_req_id") REFERENCES "user"("user_id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "loan_request" ADD CONSTRAINT "loan_request_new_loggie_id_fkey" FOREIGN KEY ("new_loggie_id") REFERENCES "user"("user_id") ON DELETE SET NULL ON UPDATE CASCADE;
